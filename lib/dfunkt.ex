97d49540778aec907a3a01ff57b6ec929f7980a0
Andreas Mårtensson <addem1234@gmail.com>
2017-05-18 18:41:03 +0200

    New dfunkt url

--- lib/dfunkt.ex
+++ lib/dfunkt.ex
@@ -2,7 +2,7 @@ defmodule Pls.Dfunkt do
   import HTTPotion
 
   def get_mandates uid do
-    res = get("http://dfunkt.froyo.datasektionen.se/api/user/kthid/" <> uid <> "/current")
+    res = get("https://dfunkt.datasektionen.se/api/user/kthid/" <> uid <> "/current")
 
     json = case Poison.decode(res.body) do
       {:ok, json} -> json
